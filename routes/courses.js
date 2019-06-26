dddff585e36c43e19c1eee6f77be2eb38570a069
Akram Imtiaz <akram.imtiaz10@gmail.com>
2019-06-27 02:54:01 +1000

    Add Courses GET all records route

--- routes/courses.js
+++ routes/courses.js
@@ -1,8 +1,37 @@
 const express = require('express');
 const router = express.Router();
 
+const { sequelize, models } = require('../db');
+const { Course } = models;
+
+const bcryptjs = require('bcryptjs');
+const authenticateUser = require('./misc/authenticate');
+
+const { check, validationResult } = require('express-validator/check');
+
+//RETRIEVE ALL
 router.get('/', (req, res) => {
+    Course.findAll({
+        attributes: ['id', 'title', 'description', 'estimatedTime', 'materialsNeeded', 'userId']
+    })
+    .then(courses => res.status(200).json(courses))
+    .catch(error => console.log(error));
+});
+//READ
+router.get('/:id', (req, res) => {
 
 });
+//CREATE
+router.post('/', (req, res) => {
+
+});
+//UPDATE
+router.put('/:id', (req, res) => {
+
+})
+//DELETE
+router.delete('/:id', (req, res) => {
+    
+})
 
 module.exports = router;
